25e0b91763fbb15fdbf2ec3a0653c076ba35393f
Lukeeeeee <lukedong123@gmail.com>
2019-02-18 23:27:33 +0800

    add garage space

---
+++ mbrl/common/spaces/__init__.py
@@ -0,0 +1,7 @@
+from mbrl.common.spaces.base import Space
+from mbrl.common.spaces.box import Box
+from mbrl.common.spaces.dict import Dict
+from mbrl.common.spaces.discrete import Discrete
+from mbrl.common.spaces.tuple import Tuple
+
+__all__ = ["Space", "Box", "Dict", "Discrete", "Tuple"]
