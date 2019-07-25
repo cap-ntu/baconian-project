4ab364af96732e9dd50d4cdd32746003b68a2b2c
Shen Chen <shenc.96@gmail.com>
2019-07-25 20:54:29 +0800

    Modified ensemble implementation to offload model choice to external user. Also modified implementation to execute train() function for models provided.

--- baconian/core/ensemble.py
+++ baconian/core/ensemble.py
@@ -19,39 +19,36 @@ class Ensemble(Basic):
 
 class ModelEnsemble(Ensemble, DynamicsModel):
 
-    def __init__(self, n_models=1, prediction='random', *args, **kwargs):
+    def __init__(self, model, n_models=1, prediction_type='random', *args, **kwargs):
         """
         :param env_spec:
         :param parameters:
         :param init_state:
         :param name:
         :param n_models:
-        :param prediction:
+        :param prediction_type:
         """
 
         super().__init__(*args, **kwargs)
-        self._prediction = prediction
+
+        self._prediction_type = prediction_type
         self._observations = list()
         self._model = list()
         self._name = kwargs.pop('name', 'dynamics_model')
 
-        for num in range(n_models):
-            self._model.append(DynamicsModel(*args, **dict(kwargs, name=self._name + '_' + num)))
-
-    def fit(self, *z, **fit_params):
-        """
-        Fit the ``idx``-th model of the ensemble if ``idx`` is provided, a
-        random model otherwise.
-        Args:
-            *z (list): a list containing the inputs to use to predict with each
-                regressor of the ensemble;
-            **fit_params (dict): other params.
-        """
-        idx = fit_params.pop('idx', None)
-        if idx is None:
-            self[np.random.choice(len(self))].fit(*z, **fit_params)
+        if isinstance(model, DynamicsModel):
+            for a in range(n_models):
+                self._model.append(model.make_copy())
         else:
-            self[idx].fit(*z, **fit_params)
+            base_env_spec = model[0]
+            for b in range(len(model)):
+                if model[b].env_spec != base_env_spec:
+                    raise TypeError('EnvSpec of list of models do not match.')
+            self._model = model
+
+    def train(self, *args, **kwargs):
+        for idx in range(len(self._model)):
+            self._model[idx].train(*args, **kwargs)
 
     def reset_state(self, *args, **kwargs):
         """
@@ -72,11 +69,9 @@ class ModelEnsemble(Ensemble, DynamicsModel):
         for m in self.model:
             self._observations.append(m.step(*args, **kwargs))
 
-        if self._prediction == 'mean':
+        if self._prediction_type == 'mean':
             results = np.mean(self._observations, axis=0)
-        elif self._prediction == 'sum':
-            results = np.sum(self._observations, axis=0)
-        elif self._prediction == 'random':
+        elif self._prediction_type == 'random':
             results = self._observations[np.random.randint(low=0, high=len(self.model))]
         else:
             raise ValueError
@@ -125,6 +120,19 @@ class ModelEnsemble(Ensemble, DynamicsModel):
 
 class AlgoEnsemble(Ensemble, Algo):
 
+    def __init__(self, cls, n_algos, prediction_type='random', *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self._cls = cls
+        self._n_algos = n_algos
+        self._prediction_type = prediction_type
+        self._predictions = list()
+        self._algo = list()
+        self._name = kwargs.pop('name', 'algo')
+
+        for num in range(n_algos):
+            self._algo.append(cls(*args, **dict(kwargs, name=self._name + '_' + num)))
+
     def init(self, *args, **kwargs):
         raise NotImplementedError
 
@@ -134,3 +142,16 @@ class AlgoEnsemble(Ensemble, Algo):
     def load(self, *args, **kwargs):
         raise NotImplementedError
 
+    @property
+    def algo(self):
+        """
+        Returns:
+            The list of the models in the ensemble.
+        """
+        return self._algo
+
+    def __len__(self):
+        return len(self._algo)
+
+    def __getitem__(self, idx):
+        return self._algo[idx]
