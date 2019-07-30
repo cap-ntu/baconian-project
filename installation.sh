ffb008f668748b568ea4f5a0d2ddac6d81fb971b
DavidChenL <3439388084@qq.com>
2019-07-30 06:59:35 -0700

    roboschool configuration for Ubuntu 14.04

--- installation.sh
+++ installation.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+sudo apt-get install libgl1-mesa-dev
 sudo apt-get install libpcre3
-apt-get install libpcre3-dev
+sudo apt-get install libpcre3-dev
+
 pip install pip==9.0.1
 pip install -e .
