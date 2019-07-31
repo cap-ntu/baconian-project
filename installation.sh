f863b9bbf5380e44724e5f769b1265ed7ddf1685
DavidChenL <3439388084@qq.com>
2019-07-31 01:43:16 -0700

    Removed Ubuntu 14.04 testing

--- installation.sh
+++ installation.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-sudo apt-get install libgl1-mesa-dev
-sudo apt-get install libpcre3
-sudo apt-get install libpcre3-dev
-
 pip install pip==9.0.1
-pip install roboschool
 pip install -e .
