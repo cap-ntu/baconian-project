bef255e0fe09efed1d3e361f05e06ac7e54abd96
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2019-09-25 15:04:01 +0800

    Tuned hyperparameters for DQN

--- examples/dqn_acrobat_example.py
+++ examples/dqn_acrobat_example.py
@@ -8,22 +8,36 @@ from baconian.core.experiment import Experiment
 from baconian.core.flow.train_test_flow import create_train_test_flow
 from baconian.config.global_config import GlobalConfig
 
-
 def task_fn():
     env = make('Acrobot-v1')
     name = 'demo_exp'
     env_spec = EnvSpec(obs_space=env.observation_space,
                        action_space=env.action_space)
-
     mlp_q = MLPQValueFunction(env_spec=env_spec,
                               name_scope=name + '_mlp_q',
                               name=name + '_mlp_q',
                               mlp_config=[
                                   {
-                                      "ACT": "RELU",
+                                      "ACT": "TANH",
                                       "B_INIT_VALUE": 0.0,
                                       "NAME": "1",
-                                      "N_UNITS": 16,
+                                      "N_UNITS": 64,
+                                      "TYPE": "DENSE",
+                                      "W_NORMAL_STDDEV": 0.03
+                                  },
+                                  {
+                                      "ACT": "TANH",
+                                      "B_INIT_VALUE": 0.0,
+                                      "NAME": "2",
+                                      "N_UNITS": 64,
+                                      "TYPE": "DENSE",
+                                      "W_NORMAL_STDDEV": 0.03
+                                  },
+                                  {
+                                      "ACT": "RELU",
+                                      "B_INIT_VALUE": 0.0,
+                                      "NAME": "3",
+                                      "N_UNITS": 256,
                                       "TYPE": "DENSE",
                                       "W_NORMAL_STDDEV": 0.03
                                   },
@@ -37,34 +51,31 @@ def task_fn():
                                   }
                               ])
     dqn = DQN(env_spec=env_spec,
-              config_or_config_dict=dict(REPLAY_BUFFER_SIZE=1000,
+              config_or_config_dict=dict(REPLAY_BUFFER_SIZE=50000,
                                          GAMMA=0.99,
-                                         BATCH_SIZE=10,
-                                         LEARNING_RATE=0.01,
-                                         TRAIN_ITERATION=1,
+                                         BATCH_SIZE=32,
+                                         LEARNING_RATE=0.001,
+                                         TRAIN_ITERATION=10,
                                          DECAY=0.5),
               name=name + '_dqn',
               value_func=mlp_q)
-
     agent = Agent(env=env, env_spec=env_spec,
                   algo=dqn,
                   name=name + '_agent',
                   exploration_strategy=EpsilonGreedy(action_space=env_spec.action_space,
                                                      init_random_prob=0.5))
-
     flow = create_train_test_flow(
-        test_every_sample_count=10,
-        train_every_sample_count=10,
-        start_test_after_sample_count=5,
-        start_train_after_sample_count=5,
+        test_every_sample_count=100,
+        train_every_sample_count=1,
+        start_test_after_sample_count=1000,
+        start_train_after_sample_count=1000,
         train_func_and_args=(agent.train, (), dict()),
-        test_func_and_args=(agent.test, (), dict(sample_count=10)),
-        sample_func_and_args=(agent.sample, (), dict(sample_count=100,
+        test_func_and_args=(agent.test, (), dict(sample_count=1, sample_trajectory_flag=True)),
+        sample_func_and_args=(agent.sample, (), dict(sample_count=1,
                                                      env=agent.env,
                                                      in_which_status='TRAIN',
                                                      store_flag=True))
     )
-
     experiment = Experiment(
         tuner=None,
         env=env,
@@ -74,8 +85,7 @@ def task_fn():
     )
     experiment.run()
 
-
 from baconian.core.experiment_runner import *
 
-GlobalConfig().set('DEFAULT_LOG_PATH', './log_path2')
+GlobalConfig().set('DEFAULT_LOG_PATH', './log_path')
 single_exp_runner(task_fn, del_if_log_path_existed=True)
